ce1f8f94d826bde3888069310d531e5fe71c36f2
Nigam Puskar <nigampuskar02@gmail.com>
2025-04-27 16:43:48 +0100

    testing

--- src/Solvers/Solvers_1_CSIMPLE.jl
+++ src/Solvers/Solvers_1_CSIMPLE.jl
@@ -44,8 +44,7 @@ function setup_compressible_solvers(
 
     (; solvers, schemes, runtime, hardware) = config
 
-    @info "Extracting configuration and input fields..."
-    println("reached here!")
+    @info "Extracting configuration and input field..."
 
     # model = adapt(hardware.backend, model_in)
     (; U, p) = model.momentum
